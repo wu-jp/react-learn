889410a7d1361a3a05b692c49dffd1b6dc88eb1c
wu-jp <13064157803@163.com>
2021-07-26 07:24:24 +0800

    自建传递元素内容学习代码

---
+++ src/Comp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Comp(props) {
+    console.log(props)
+    return (
+        <div className="Comp">
+            <h1>组件自身的内容</h1>
+            {props.children || <h1>'默认值'</h1>}
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Comp from './Comp'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <h1>hello react!</h1>
-    </React.StrictMode>,
+    <Comp>
+        <h1>这里是父组件传递的组件内容用</h1>
+    </Comp>,
     document.getElementById('root')
 )
