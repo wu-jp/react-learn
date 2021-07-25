5ff5fb8510b599fb05653b4561b3284d7bfe41e0
wu-jp <13064157803@163.com>
2021-07-25 09:09:32 +0800

    one

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-ReactDOM.render(
-    <React.StrictMode>
-        <h1>hello react!</h1>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
