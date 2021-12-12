a818de5a7902343558cacb6daaf7250c72b63a52
wu-jp <13064157803@163.com>
2021-12-12 19:08:00 +0800

    docs: 新增代码注释、删除多余的导入

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import ReactDOM, { flushSync } from 'react-dom'
-import src1 from './assets/1.png'
-import src2 from './assets/2.png'
-import src3 from './assets/3.png'
+import React from "react"
+import ReactDOM from "react-dom"
+import src1 from "./assets/1.png"
+import src2 from "./assets/2.png"
+import src3 from "./assets/3.png"
 
 const imgs = [src1, src2, src3]
 
 let index = 0
 let timer
 
-const container = document.getElementById('root')
+const container = document.getElementById("root")
 
 function render() {
-    ReactDOM.render(<img src={imgs[index]} alt="" />, container)
+  ReactDOM.render(<img src={imgs[index]} alt="" />, container)
 }
 
 function start() {
-    stop()
-    timer = setInterval(() => {
-        index = (index + 1) % 3
-        render()
-    }, 2000)
+  stop()
+  timer = setInterval(() => {
+    index = (index + 1) % 3
+    render()
+  }, 2000)
 }
 
 function stop() {
-    clearInterval(timer)
+  clearInterval(timer)
 }
 render()
 start()
 
+// 当指针移动到元素上时，触发事件
 container.onmouseenter = function () {
-    stop()
+  stop()
 }
+// 当指针从元素上移出时，触发事件
 container.onmouseleave = function () {
-    start()
+  start()
 }
