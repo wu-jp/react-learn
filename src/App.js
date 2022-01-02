f5a92cb12643ba3f18ae9007716cc9085cf7cec5
wu-jp <13064157803@163.com>
2022-01-02 11:34:44 +0800

    docs: 学习React中的Portals(插槽)使用

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react"
-// import Test from "./components/test" // Render Props
-import Test from "./components/test copy" //HOC
-export default function App() {
+import ReactDOM from "react-dom"
+
+function ChildA() {
+  // ReactDOM.createPortal()
+  // 第一个参数为：要渲染的React元素
+  // 第二个参数为：要插入的真实DOM地址
+  // 返回值：第一个参数
+  return ReactDOM.createPortal(
+    <div>
+      <h1>ChildA</h1>
+      <ChildB />
+    </div>,
+    document.querySelector(".modal")
+  )
+}
+
+function ChildB() {
   return (
     <div>
-      <Test />
+      <h1>ChildB</h1>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div
+      onClick={e => {
+        console.log("这里是App组件", e.target)
+      }}
+    >
+      <h1>App</h1>
+      <ChildA />
     </div>
   )
 }
--- src/components/HOCMouseTest/ShowMousePoint.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function ShowMousePoint(props) {
-  return (
-    <div>
-      当前坐标点x:{props.x} y:{props.y}
-    </div>
-  )
-}
