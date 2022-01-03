9524d3bdefded06ded5da5251435f28195b38a88
wu-jp <13064157803@163.com>
2022-01-03 08:54:31 +0800

    docs: 学习react中的事件处理

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
 import React from "react"
-import ErrorBound from "./components/ErrorBound"
-
-function ChildA() {
-  return (
-    <div
-      style={{
-        height: 350,
-        width: 350,
-        border: "2px solid green",
-      }}
-    >
-      <h1>ChildA</h1>
-      <ChildB />
-    </div>
-  )
-}
-
-function ChildB() {
-  // 模拟错误
-  let arr = undefined
-  let arrLi = arr.map(item => <li key={item}>{item}</li>)
-  return (
-    <div
-      style={{
-        height: 200,
-        width: 200,
-        border: "2px solid red",
-      }}
-    >
-      {arrLi}
-      <h1>ChildB</h1>
-    </div>
-  )
-}
-
-function ChildC() {
-  return (
-    <div>
-      <h1>这里是ChildC组件</h1>
-    </div>
-  )
-}
 
 export default function App() {
   return (
-    <div
-      style={{
-        height: 500,
-        width: 500,
-        border: "2px solid",
-      }}
-    >
+    <div>
       <h1>App组件</h1>
-      <ChildC />
-      <ErrorBound>
-        <ChildA />
-      </ErrorBound>
+      <button
+        onClick={() => {
+          console.log("react: 点击了button")
+        }}
+      >
+        点击
+      </button>
     </div>
   )
 }
+document.querySelector("#root").addEventListener("click", function () {
+  console.log("真实DOM：监听到了点击事件")
+})
+
+// 在react事件处理中：会将react事件冒泡到document元素中进行处理
