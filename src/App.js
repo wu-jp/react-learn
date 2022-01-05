28e64e5ec864a0790a417ce6a28262d70ff51d60
wu-jp <13064157803@163.com>
2022-01-05 19:21:55 +0800

    docs: 学习React渲染原理

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ export default function App() {
   return (
     <div>
       <h1>App组件</h1>
-      <button
-        onClick={() => {
-          console.log("react: 点击了button")
-        }}
-      >
-        点击
-      </button>
     </div>
   )
 }
-document.querySelector("#root").addEventListener("click", function () {
-  console.log("真实DOM：监听到了点击事件")
-})
-
-// 在react事件处理中：会将react事件冒泡到document元素中进行处理
