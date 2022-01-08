cfbdcc8a892910906916fd0a4f0fa25c0f9adea4
wu-jp <13064157803@163.com>
2022-01-08 23:15:34 +0800

    docs: 学习Callback Hook，用于优化性能

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React, { useContext } from "react"
-import { ctx } from "./components/Context"
-import Test from "./components/test"
+import React, { useState, useCallback } from "react"
+
+class Test extends React.PureComponent {
+  render() {
+    console.log("Test Render")
+    return (
+      <div>
+        <p>text: {this.props.text}</p>
+        <button
+          onClick={() => {
+            this.props.onClick()
+          }}
+        >
+          改变文字
+        </button>
+      </div>
+    )
+  }
+}
+
+function Parent() {
+  console.log("Parent Render")
+  const [txt, setTxt] = useState(1)
+  const [num, setNum] = useState(0)
+  const fn = () => {
+    setTxt(Math.random())
+  }
+  const handleClick = useCallback(fn, [])
+  return (
+    <div>
+      <Test text={txt} onClick={handleClick} />
+      <input
+        type="number"
+        value={num}
+        onChange={e => {
+          setNum(e.target.value)
+        }}
+      />
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <div>
-      <ctx.Provider value="abc">
-        <Test />
-      </ctx.Provider>
+      <Parent />
     </div>
   )
 }
--- src/components/Context.js
+++
@@ -1,4 +0,0 @@
-import React from "react"
-
-// 创建test公共上下文对象
-export const ctx = React.createContext()
