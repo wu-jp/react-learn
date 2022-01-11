4e87ab7aa7561c5283c9e918256f08489f962405
wu-jp <13064157803@163.com>
2022-01-11 21:01:29 +0800

    docs: 学习react动画库react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { useLayoutEffect, useState, useRef, useEffect, useDebugValue } from "react"
+import React, { useState } from "react"
+import { Transition } from "react-transition-group"
 
-function useTest() {
-  // useDebugValue用于调试
-  useDebugValue("学生列表")
-  const [students] = useState([])
-  return students
+const duration = 1000
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
 }
 
 export default function App() {
-  useState(0)
-  useRef()
-  useLayoutEffect(() => {
-    return () => {}
-  })
-  useEffect(() => {
-    return () => {}
-  })
-  useTest()
-  return <div></div>
+  const [inProp, setInProp] = useState(true)
+  return (
+    <div>
+      <Transition in={inProp} timeout={duration}>
+        {state => {
+          console.log(state)
+          return (
+            <div
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[state],
+              }}
+            >
+              I'm a fade Transition!
+            </div>
+          )
+        }}
+      </Transition>
+      <button onClick={() => setInProp(!inProp)}>Click to Enter</button>
+    </div>
+  )
 }
