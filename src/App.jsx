30899d17b34961fd1a03f89c69f1fc8bf2a21553
wu-jp <13064157803@163.com>
2022-01-22 23:51:36 +0800

    docs: 学习react-router v6，并练习demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,117 @@
-import { useState } from 'react'
-import {Link, Outlet} from 'react-router-dom'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React, { useState } from "react"
+import { Routes, Route, Link, useSearchParams } from "react-router-dom"
+export default function App() {
   return (
     <div className="App">
-      <h1>Bookkeeper!</h1>
-      <nav style={{
-        borderBottom: 'solid 1px',
-        paddingBottom: '1rem'
-      }}>
-        <Link to="/invoices">Invoices</Link> | {''}
-        <Link to="/expenses">Expenses</Link>
-      </nav>
-      <Outlet />
+      <h1>Search Params Example</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   )
 }
 
-export default App
+function randomUser() {
+  let users = ["chaance", "jacob-ebey", "mcansh", "mjackson", "ryanflorence", "wu-jp", "wopen"]
+  return users[Math.floor(Math.random() * users.length)]
+}
+
+function Home() {
+  let [searchParams, setSearchParams] = useSearchParams()
+  let user = searchParams.get("user")
+  let [userData, setUserData] = useState(null)
+
+  React.useEffect(() => {
+    console.log("执行副作用")
+    let abortController = new AbortController()
+
+    async function getGitHubUser() {
+      let response = await fetch(`https://api.github.com/users/${user}`, {
+        signal: abortController.signal,
+      })
+      if (!abortController.signal.aborted) {
+        let data = await response.json()
+        setUserData(data)
+      }
+    }
+
+    if (user) {
+      getGitHubUser()
+    }
+    return () => {
+      console.log("清除副作用")
+      abortController.abort()
+    }
+  }, [user])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    let formData = new FormData(event.currentTarget)
+    let newUser = formData.get("user")
+    if (!newUser) return
+    setSearchParams({ user: newUser })
+  }
+  function handleRandomSubmit(event) {
+    event.preventDefault()
+    let newUser = randomUser()
+    // our new random user is the same as our current one, let's try again
+    if (newUser === user) {
+      handleRandomSubmit(event)
+    } else {
+      setSearchParams({ user: newUser })
+    }
+  }
+
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input defaultValue={user ?? undefined} type="text" name="user" />
+            <button type="submit">Search</button>
+          </label>
+        </form>
+        <form onSubmit={handleRandomSubmit}>
+          <input type="hidden" name="random" />
+          <button type="submit">Random</button>
+        </form>
+      </div>
+      {userData && (
+        <div
+          style={{
+            padding: "24px",
+            margin: "24px 0",
+            borderTop: "1px solid #eaeaea",
+            display: "flex",
+            alignItems: "center",
+            gap: "16px",
+          }}
+        >
+          <img
+            style={{ borderRadius: "50%" }}
+            width={200}
+            height={200}
+            src={userData.avatar_url}
+            alt={userData.login}
+          />
+          <div>
+            <h2>{userData.name}</h2>
+            <p>{userData.bio}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,15 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
-import Invoice from './routes/invoice'
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/expenses" element={<Expenses />}></Route>
-        <Route path="/invoices" element={<Invoices />}>
-          <Route
-            index
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>Select an invoice</p>
-              </main>
-            }
-          />
-          <Route path=":invoiceId" element={<Invoice />} />
-        </Route>
-        <Route path="*" element={
-          <main style={{ padding: "1rem" }}>
-            <p>There's nothing here!</p>
-          </main>
-        }></Route>
-      </Route>
-    </Routes>
-
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 )
