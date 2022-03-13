3762f787e69b0970d219fc10b8f7de17a96f7b2a
wu-jp <13064157803@163.com>
2022-03-13 16:28:57 +0800

    docs：使用redux-logger中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,12 @@
-import { createStore, bindActionCreators } from "redux"
-import applyMiddleware from "../redux/applyMiddleware" // 手写
-
+import { createStore, bindActionCreators, applyMiddleware } from "redux"
 import { updateUserAction } from "./actions/userAction"
 import { addUserAction, deleteUserAction } from "./actions/userListAction"
 import reducer from "./reducers"
 
-const log1 = function (store) {
-  return function (nextDispatch) {
-    return function (action) {
-      console.log("中间件1")
-      console.log("旧数据", store.getState())
-      console.log("action", action.type)
-      nextDispatch(action)
-      console.log("新数据", store.getState())
-      console.log("-------")
-    }
-  }
-}
-
-const log2 = store => nextDispatch => action => {
-  console.log("中间件2")
-  console.log("旧数据", store.getState())
-  console.log("action", action.type)
-  nextDispatch(action)
-  console.log("新数据", store.getState())
-  console.log("--------")
-}
+import { logger } from "redux-logger"
 
 // 方式一：
-const store = createStore(reducer, applyMiddleware(log1, log2))
+const store = createStore(reducer, applyMiddleware(logger))
 // 方式二：
 // const store = applyMiddleware(log1,log2)(createStore)(reducer)
 
