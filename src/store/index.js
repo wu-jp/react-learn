1f7d7b66aa8b51aa6ffe7b479d000f9b0fd22239
wu-jp <13064157803@163.com>
2022-03-13 16:09:51 +0800

    docs：学习redux中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,36 @@
-// import { createStore, bindActionCreators } from "redux"
-
-import createStore from "../redux/createStore" // 手写的方法
-import bindActionCreators from "../redux/bindActionCreators" // 手写的方法
+import { createStore, bindActionCreators, applyMiddleware } from "redux"
 
 import { updateUserAction } from "./actions/userAction"
 import { addUserAction, deleteUserAction } from "./actions/userListAction"
 import reducer from "./reducers"
 
-const store = createStore(reducer)
+const log1 = function (store) {
+  return function (nextDispatch) {
+    return function (action) {
+      console.log("中间件1")
+      console.log("旧数据", store.getState())
+      console.log("action", action.type)
+      nextDispatch(action)
+      console.log("新数据", store.getState())
+      console.log("-------")
+    }
+  }
+}
+
+const log2 = store => nextDispatch => action => {
+  console.log("中间件2")
+  console.log("旧数据", store.getState())
+  console.log("action", action.type)
+  nextDispatch(action)
+  console.log("新数据", store.getState())
+  console.log("--------")
+}
+
+// 方式一：
+const store = createStore(reducer, applyMiddleware(log1, log2))
+// 方式二：
+// const store = applyMiddleware(log1,log2)(createStore)(reducer)
+
 const bindAction = bindActionCreators(
   {
     updateUserAction,
@@ -23,22 +46,4 @@ const user = {
   age: 1,
 }
 
-// state监听器
-const unListen = store.subscribe(() => {
-  console.log(store.getState())
-})
-
-// 分发一个更新用户学习的action
-// store.dispatch(updateUserAction(user))
 bindAction.updateUserAction(user)
-
-// 分发一个新增用户的action
-// store.dispatch(addUserAction(user))
-bindAction.addUserAction(user)
-
-// 取消监听
-unListen()
-
-// 分发一个删除用户的action
-// store.dispatch(deleteUserAction("001"))
-bindAction.deleteUserAction("001")
