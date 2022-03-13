57636ec13fe1d2dcee1cf6d8eb6230af2b701845
wu-jp <13064157803@163.com>
2022-03-13 14:49:16 +0800

    docs: 通过bindActionCreators方法分发action

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,17 @@
-import { createStore } from "redux"
+import { createStore, bindActionCreators } from "redux"
 import { updateUserAction } from "./actions/userAction"
 import { addUserAction, deleteUserAction } from "./actions/userListAction"
 import reducer from "./reducers"
 
 const store = createStore(reducer)
+const bindAction = bindActionCreators(
+  {
+    updateUserAction,
+    addUserAction,
+    deleteUserAction,
+  },
+  store.dispatch
+)
 
 const user = {
   uuid: "001",
@@ -17,13 +25,13 @@ const unListen = store.subscribe(() => {
 })
 
 // 分发一个更新用户学习的action
-store.dispatch(updateUserAction(user))
+bindAction.updateUserAction(user)
 
 // 分发一个新增用户的action
-store.dispatch(addUserAction(user))
+bindAction.addUserAction(user)
 
 // 取消监听
 unListen()
 
 // 分发一个删除用户的action
-store.dispatch(deleteUserAction("001"))
+bindAction.deleteUserAction("001")
